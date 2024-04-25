6dd9c210c9b038162f176047b1fc77b14e66fa9e
bala <bala@vikird.com>
2024-04-26 02:46:06 +0530

    widgets sample

--- lib/screen_2.dart
+++ lib/screen_2.dart
@@ -7,14 +7,55 @@ class Screen2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: ReusableAppBar(title: 'Screen 2', toggleTheme: toggleTheme),
-        body: Center(
-          child: ElevatedButton(
-            child: const Text('Go to Screen 1'),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ));
+      appBar: ReusableAppBar(title: 'Screen 2', toggleTheme: toggleTheme),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Widgets',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            const SizedBox(height: 20.0),
+            Image.asset(
+              'assets/images/flutter_logo.png',
+              height: 100.0,
+            ),
+            const SizedBox(height: 20.0),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter your name',
+                labelText: 'Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () {
+                    // Action to perform when ElevatedButton is pressed
+                  },
+                  child: const Text('Elevated Button'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20.0),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text('Item $index'),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
