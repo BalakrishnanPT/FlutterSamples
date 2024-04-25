64e3c038b7b83dd5c3b5a2e61aa54baa6aa4cfd0
bala <bala@vikird.com>
2024-04-26 01:30:22 +0530

    Theme toggle option 1

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sample1/theme_manager.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,9 +10,12 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home Page'),
         actions: <Widget>[
-          IconButton(icon: const Icon(Icons.brightness_6), onPressed: () => {
-            
-          }),
+          IconButton(
+              icon: const Icon(Icons.brightness_6),
+              onPressed: () => {
+                    ThemeManager.toggleTheme(
+                        context) // Call the toggleTheme method
+                  }),
         ],
       ),
       body: const Center(
