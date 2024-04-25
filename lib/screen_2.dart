9ed1866677cea600c551bec6c84aeef8a2cc35ad
bala <bala@vikird.com>
2024-04-26 02:25:50 +0530

    Navigator fix

--- lib/screen_2.dart
+++ lib/screen_2.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:sample1/reusable_appbar.dart';
-import 'package:sample1/screen_1.dart';
 
 class Screen2 extends StatelessWidget {
   final Function toggleTheme;
@@ -8,18 +7,12 @@ class Screen2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: ReusableAppBar(title: 'Screen 1', toggleTheme: toggleTheme),
+        appBar: ReusableAppBar(title: 'Screen 2', toggleTheme: toggleTheme),
         body: Center(
           child: ElevatedButton(
             child: const Text('Go to Screen 1'),
             onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => Screen1(
-                          toggleTheme: toggleTheme,
-                        )),
-              );
+              Navigator.pop(context);
             },
           ),
         ));
